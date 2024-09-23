698bfcdd214dee8cdfa0a1cb9a8e5bbcb7198cfa
Nick Felker <java.2015@bk.ru>
2024-09-23 19:55:44 +0200

    added login by log name api

--- api/user/login.php
+++ api/user/login.php
@@ -1,2 +1,27 @@
 <?php
- // login api 
+include_once "../config/database.php";
+include_once "../config/user.php";
+$database = new DataBase();
+$db_connection = $database->set_connection();
+
+$user = new User();
+$user->set_connection($db_connection);
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$user->set_login($data['login']);
+$user->set_password($data['password']);
+$result = $user->login_user_by_login();
+
+if($result)
+{
+    http_response_code(200);
+    echo json_encode(array("message"=>$result));
+    return true;
+}
+else
+{
+    http_response_code(400);
+    echo json_encode(array("message"="User not found. Wrong login or password"));
+    return true;
+}
