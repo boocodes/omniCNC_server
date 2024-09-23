26cff1271039174ca99704f5431a2ffa9d2c3920
Nick Felker <java.2015@bk.ru>
2024-09-23 20:24:49 +0200

    repaired

---
+++ api/user/login_by_email.php
@@ -0,0 +1,27 @@
+<?php
+include_once "../../config/database.php";
+include_once "../../config/user.php";
+$database = new DataBase();
+$db_connection = $database->set_connection();
+
+$user = new User();
+$user->set_connection($db_connection);
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$user->set_login($data['login']);
+$user->set_email($data['email']);
+$result = $user->login_user_by_email();
+
+if($result)
+{
+    http_response_code(200);
+    echo json_encode(array("message"=>$result));
+    return true;
+}
+else
+{
+    http_response_code(400);
+    echo json_encode(array("message"=>"Wrong login or email"));
+    return true;
+}
